5f339199b7a4130b568af87c71ba1e92f62707ea
koctwba <76501232+koctwba@users.noreply.github.com>
2020-12-24 17:20:17 +0900

    12.24_17:20

--- js/main.js
+++ js/main.js
@@ -5,15 +5,33 @@ $(function(){
     $('.all_nav').click(function(){
         $('#nav_pop').fadeIn();
     })
+    $(window).scroll(function(){
+        if($(document).scrollTop() >= 800 && $(document).scrollTop
+        () <= $('#footer').offset().top - 1000){
+            $('#quickmenu').fadeIn(200)
+        }else{$('#quickmenu').fadeOut(200)}
+      })
+    $('.home').click(function(){
+        $('body,html').animate({scrollTop:0},1000);
+    })
+    $('.top').click(function(){
+        $('body,html').animate({scrollTop:0},1000);
+    })
 
     var slide;
     slide = setInterval(function(){
-        $('.notice_list > li:nth-child(1)').animate({
-            'margin-top':'-4rem'},800,function(){
-                $('.slide_box .notice_list').append($('.notice_list > li:nth-child(1)'));
-                $('.slide_box .notice_list > li:nth-child(3)').css({'margin-top':0});
-            })
-    },1200)
+        //BES .notice_list가 양쪽에 두개라서 오류가 보입니다
+        // 코드를 복사해서 이름을 다르게 주고 쓰든지 each를 추가할 필요가 있습니다 
+        $('.notice_list').each(function(){
+        
+            var this_notice = $(this);
+            var find_first = this_notice.find('li:first-child')
+             find_first.animate({'margin-top':'-2.5rem'},800,function(){
+               this_notice.append(find_first);
+               this_notice.find('li:last-child').css({'margin-top':0});   
+              }) 
+         })
+        },2500) 
     $('.clinic_img').hover(function(){
        $(this).css({'transform':'scale(1.1)'})
     },function(){
